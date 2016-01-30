1599761371a1a286b8edc13500dfcabcb84f3042
Eric Young <eric.j.young88@gmail.com>
2016-01-30 15:13:38 -0700

    Initial server listening code

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,30 @@
 mod voodoo;
 extern crate hyper;
 
+use std::sync::Mutex;
 use std::sync::mpsc::{channel, Sender, Receiver};
 use hyper::server::{Handler, Server, Request, Response};
-use voodoo::GameManager;
+use voodoo::{GameManager, VoodooMessage};
 
 fn main() {
-    GameManager::run();
+    let manager_sender = GameManager::run();
+    Server::http("0.0.0.0:9282").unwrap().handle(NetworkHandler::new(manager_sender)).unwrap();
+}
+
+struct NetworkHandler {
+    manager_sender: Mutex<Sender<VoodooMessage>>
+}
+
+impl NetworkHandler {
+    fn new(sender: Sender<VoodooMessage>) -> NetworkHandler {
+        NetworkHandler {
+            manager_sender: Mutex::new(sender)
+        }
+    }
+}
+
+impl Handler for NetworkHandler {
+    fn handle(&self, request: Request, response: Response) {
+        response.send("It's alive".as_bytes()).unwrap();
+    }
 }
