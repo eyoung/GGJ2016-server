9591f52f6d706a4e6bc5ef238f2785746d7ff265
Eric Young <eric.j.young88@gmail.com>
2016-01-30 16:03:20 -0700

    Log body for debug

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ extern crate hyper;
 
 use std::sync::Mutex;
 use std::sync::mpsc::{channel, Sender, Receiver};
+use std::io::Read;
 use hyper::server::{Handler, Server, Request, Response};
 use voodoo::{GameManager, VoodooMessage};
 
@@ -24,7 +25,12 @@ impl NetworkHandler {
 }
 
 impl Handler for NetworkHandler {
-    fn handle(&self, request: Request, response: Response) {
-        response.send("It's alive".as_bytes()).unwrap();
+    fn handle(&self, mut request: Request, response: Response) {
+        let mut request_buffer = Vec::new();
+        let request_body = request.read_to_end(&mut request_buffer);
+        if let Ok(body_content) = String::from_utf8(request_buffer) {
+            println!("Requested with body {}", &body_content);
+        }
+        response.send(format!("{}\n", "it's alive").as_bytes()).unwrap();
     }
 }
